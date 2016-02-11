5d6140ea73a33059246c6b5ebac17d28fbd1ea1e
Alex Minge <aminge37@gmail.com>
2016-02-11 17:11:38 -0600

    First version of group assignment.

--- public/scripts/clientapp.js
+++ public/scripts/clientapp.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-    $('#post-data').on('click', clickPostData);
-    $('#get-data').on('click', clickGetData);
+    $('#post-name-data').on('click', clickPostNameData);
+    $('#get-name-data').on('click', clickGetNameData);
+
+    $('#post-animal-data').on('click', clickPostAnimalData);
+    $('#get-animal-data').on('click', clickGetAnimalData);
 
 });
 
-function clickPostData() {
+function clickPostNameData() {
     event.preventDefault();
     var values = {};
-    $.each($('#post-form').serializeArray(), function(i, field) {
+    $.each($('#post-name-form').serializeArray(), function(i, field) {
         values[field.name] = field.value;
     });
 
-    $('#post-form').find('input[type=text]').val('');
+    $('#post-name-form').find('input[type=text]').val('');
 
     $.ajax({
         type: 'POST',
@@ -25,11 +28,43 @@ function clickPostData() {
             console.log(data);
         }
     });
-
-
 }
 
-function clickGetData() {
+function clickGetNameData() {
+    event.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: '/data',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+}
+
+function clickPostAnimalData() {
+    event.preventDefault();
+    var values = {};
+    $.each($('#post-animal-form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    $('#post-animal-form').find('input[type=text]').val('');
+
+    $.ajax({
+        type: 'POST',
+        url: '/data',
+        data: values,
+        beforeSend: function() {
+            console.log('before!');
+        },
+        success: function(data) {
+            console.log('From Server: ', data);
+            console.log(data);
+        }
+    });
+}
+
+function clickGetAnimalData() {
     event.preventDefault();
     $.ajax({
         type: 'GET',
---
+++ routes/name.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var nameArray = [];
+
+function randomIndex(array){
+    var index = randomNumber(0, array.length);
+    return array[index];
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+router.get('/', function(req, res) {
+    res.send({message: 'hello'});
+});
+
+router.post('/', function(req, res) {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+module.exports = router;
